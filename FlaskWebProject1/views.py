e4a6aeaf9a52c2165088bedf886fdbbc571f7514
speark <speark0438@gmail.com>
2017-03-21 14:08:27 +0900

    fileupload_html

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -1,14 +1,50 @@
 """
 Routes and views for the flask application.
 """
-
+import os
 from datetime import datetime
-from flask import render_template
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 from FlaskWebProject1 import app
 
-@app.route('/')
+#app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'c:/cmk' #'d:/home/site'
+
+UPLOAD_FOLDER = 'c:/cmk'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'good!'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+        
 @app.route('/home')
 def home():
+    
     """Renders the home page."""
     return render_template(
         'index.html',
